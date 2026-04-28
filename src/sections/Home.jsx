856d7799292abbd030d33568e685194223cad902
sukeerthiii <227295636+sukeerthiii@users.noreply.github.com>
2026-04-27 21:51:01 -0700

    Add crayon flower assets

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,3 +1,7 @@
+import flowerBlue from '../assets/flowers/flower-blue.png'
+import flowerPink from '../assets/flowers/flower-pink.png'
+import flowerYellow from '../assets/flowers/flower-yellow.png'
+
 const underlined = {
   textDecoration: 'underline',
   textUnderlineOffset: '0.18em',
@@ -18,7 +22,16 @@ export default function Home() {
         Sukeerthi
       </h1>
 
-      <p>
+      <p className="intro-line">
+        <span>Hi, I'm Sukeerthi</span>
+        <span className="intro-flower-row" aria-hidden="true">
+          <img className="intro-flower intro-flower-yellow" src={flowerYellow} alt="" />
+          <img className="intro-flower intro-flower-pink" src={flowerPink} alt="" />
+          <img className="intro-flower intro-flower-blue" src={flowerBlue} alt="" />
+        </span>
+      </p>
+
+      <p style={{ marginTop: 14 }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae
         justo sed neque aliquet posuere.
       </p>
